032ffacdfe490b582eb4dd5b6fb4dd9205f4c52b
toxic-johann <toxic.johann@gmail.com>
2015-01-07 19:52:37 +0800

    8888=>8765

--- gitGuard.py
+++ gitGuard.py
@@ -15,6 +15,6 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(8765)
     tornado.ioloop.IOLoop.instance().start()
 
