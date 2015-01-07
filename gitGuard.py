6305b3d9e8529830110ba980e3849135f4b34c00
toxic-johann <toxic.johann@gmail.com>
2015-01-07 19:45:56 +0800

    set the git pull gaurd

---
+++ gitGuard.py
@@ -0,0 +1,20 @@
+import subprocess
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def git(*args):
+        return subprocess.check_call(['git'] + list(args))
+
+    def get(self):
+        self.write("receive")
+        git('pull')
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
