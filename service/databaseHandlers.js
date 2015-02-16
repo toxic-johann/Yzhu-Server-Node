abd39022ea1f7223e6114621a173d1f9fb2d569b
toxic-johann <toxic.johann@gmail.com>
2015-02-16 22:38:28 +0800

    【】

--- service/databaseHandlers.js
+++ service/databaseHandlers.js
@@ -1441,7 +1441,7 @@ function getSolicitList(fields,callback){
 
 	redisClient.ZRANGEBYSCORE(fields.kind+":"+fields.userId+":solicit","-inf","+inf","WITHSCORES",function(err,reply){
 		if(!err){
-			var result;
+			var result=[];
 			for(var i=reply.length-1;i>-1;i=i-2){
 				if(i%2 === 1){
 					result[(i-1)/2] = {};
